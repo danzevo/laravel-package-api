ff0da972148a4bd8b507682c2263804038719138
dani nugrahadi pradana <dani.nugrahadi@gmail.com>
2022-01-15 11:23:33 +0700

    feat(unitTest)

---
+++ tests/Feature/CrudapiTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CrudapiTest extends TestCase
+{
+    use withFaker;
+    /**
+     *
+     * @test
+     * @return void
+     */
+    public function it_index()
+    {
+        $response = $this->get('/api');
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     *
+     * @test
+     * @return void
+     */
+    public function it_store()
+    {
+        $response = $this->post('/api/posts', [
+                        'title' => $this->faker->words(5, true),
+                        'body' => $this->faker->words(20, true),
+                        'userId' => $this->faker->randomNumber(1),
+                    ]);
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     *
+     * @test
+     * @return void
+     */
+    public function it_update()
+    {
+        $response = $this->patch('/api/posts/'.$this->faker->randomNumber(1), [
+                        'title' => $this->faker->words(5, true),
+                        'body' => $this->faker->words(20, true),
+                        'userId' => $this->faker->randomNumber(1),
+                    ]);
+
+        $response->assertStatus(200);
+    }
+
+
+    /**
+     *
+     * @test
+     * @return void
+     */
+    public function it_delete()
+    {
+        $response = $this->delete('/api/posts/'.$this->faker->randomNumber(1));
+
+        $response->assertStatus(200);
+    }
+}
